a08d63aefaafe775b72220656ef38f986dcdd023
GidSamina <gidon.samina@gmail.com>
2025-03-01 11:19:56 +0200

    add new file Utils.py

---
+++ Utils.py
@@ -0,0 +1,11 @@
+import os
+
+# A string representing a file name. By default “Scores.txt”
+SCORES_FILE_NAME = 'Scores.txt'
+# A number representing a bad return code for a function.
+BAD_RETURN_CODE = -1
+# Screen_cleaner - A function to clear the screen (useful when playing memory game or
+# before a new game starts).
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear')
+clear_screen()
